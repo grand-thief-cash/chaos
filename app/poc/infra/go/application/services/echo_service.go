bf4498ae494213265afdd40e9fff7025ce13076c
Chanter-Machine <992400463@qq.com>
2025-09-18 00:02:05 +0800

    - v0.6.2   - poc/application: Added mysql, redis dependencies[Not tested]

--- app/poc/infra/go/application/services/echo_service.go
+++ app/poc/infra/go/application/services/echo_service.go
@@ -4,11 +4,23 @@ package services
 import (
 	"context"
 
+	"github.com/grand-thief-cash/chaos/app/infra/go/application/components/mysql"
+	"github.com/grand-thief-cash/chaos/app/infra/go/application/components/redis"
 	pb "github.com/grand-thief-cash/chaos/app/infra/go/common/grpc_gen/poc"
 )
 
 type EchoService struct {
 	pb.UnimplementedEchoServiceServer
+	MySQL *mysql.MysqlComponent
+	Redis *redis.RedisComponent
+	// 其他依赖...
+}
+
+func NewEchoService(mysql *mysql.MysqlComponent, redis *redis.RedisComponent) *EchoService {
+	return &EchoService{
+		MySQL: mysql,
+		Redis: redis,
+	}
 }
 
 func (s *EchoService) Say(ctx context.Context, req *pb.EchoRequest) (*pb.EchoReply, error) {
