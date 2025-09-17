87efe2b43b9d850e2588a49a4ba942d01dc2dc88
Chanter-Machine <992400463@qq.com>
2025-09-18 00:02:05 +0800

    - v0.6.2   - poc/application: Added mysql, redis dependencies[Not tested]

--- app/poc/infra/go/application/services/init.go
+++ app/poc/infra/go/application/services/init.go
@@ -2,18 +2,23 @@
 package services
 
 import (
-	"fmt"
-
 	"github.com/grand-thief-cash/chaos/app/infra/go/application/components/grpc_server"
+	"github.com/grand-thief-cash/chaos/app/infra/go/application/components/mysql"
+	"github.com/grand-thief-cash/chaos/app/infra/go/application/components/redis"
+	"github.com/grand-thief-cash/chaos/app/infra/go/application/consts"
 	"github.com/grand-thief-cash/chaos/app/infra/go/application/core"
 	pb "github.com/grand-thief-cash/chaos/app/infra/go/common/grpc_gen/poc"
 	"google.golang.org/grpc"
 )
 
 func init() {
-	fmt.Println("init executed: registering EchoService")
 	grpc_server.RegisterService(func(s *grpc.Server, c *core.Container) error {
-		pb.RegisterEchoServiceServer(s, &EchoService{})
+		mysqlComp, _ := c.Resolve(consts.COMPONENT_MYSQL)
+		redisComp, _ := c.Resolve(consts.COMPONENT_REDIS)
+		pb.RegisterEchoServiceServer(s, NewEchoService(
+			mysqlComp.(*mysql.MysqlComponent),
+			redisComp.(*redis.RedisComponent),
+		))
 		return nil
 	})
 }
