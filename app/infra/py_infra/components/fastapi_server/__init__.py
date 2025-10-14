201f394bb7bf8dd05ee4c96b55fcdd426535d6b6
Chanter-Machine <992400463@qq.com>
2025-10-14 16:12:27 +0800

    remove py_infra

--- app/infra/py_infra/components/fastapi_server/__init__.py
+++
@@ -1,6 +0,0 @@
-# components/fastapi_server/__init__.py
-from .component import FastAPIServerComponent
-from .schema import FastAPIServerConfig
-from .factory import create_and_register_fastapi_server
-
-__all__ = ["FastAPIServerComponent", "FastAPIServerConfig", "create_and_register_fastapi_server"]
--- app/infra/py_infra/components/logging/__init__.py
+++
@@ -1,6 +0,0 @@
-# components/logging/__init__.py
-from components.logging.factory import create_and_register_logging
-
-def setup_logging(config: dict, app_name: str):
-    """模块入口：初始化并注册日志系统"""
-    return create_and_register_logging(config, app_name)
