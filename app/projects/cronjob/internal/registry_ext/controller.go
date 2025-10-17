f2f9ac4f683bec29798211fd26a3aab6d1c60274
Chanter-Machine <992400463@qq.com>
2025-10-17 21:53:02 +0800

    - v0.4.2   - Replaced  RegisterAutoInferName with registry.RegisterAuto.

--- app/projects/cronjob/internal/registry_ext/controller.go
+++ app/projects/cronjob/internal/registry_ext/controller.go
@@ -6,14 +6,13 @@ import (
 	"github.com/grand-thief-cash/chaos/app/infra/go/application/core"
 	"github.com/grand-thief-cash/chaos/app/infra/go/application/registry"
 	"github.com/grand-thief-cash/chaos/app/projects/cronjob/internal/api"
-	"github.com/grand-thief-cash/chaos/app/projects/cronjob/internal/consts"
 )
 
 func init() {
 	// Ensure http_server starts after our controller component by extending its runtime dep graph.
-	registry.ExtendRuntimeDependencies(appconsts.COMPONENT_HTTP_SERVER, consts.COMP_CTRL_TASK_MGMT)
+	registry.ExtendRuntimeDependencies(appconsts.COMPONENT_HTTP_SERVER, api.NewTaskMgmtController().Name())
 
-	registry.RegisterAuto(consts.COMP_CTRL_TASK_MGMT, func(cfg *config.AppConfig, c *core.Container) (bool, core.Component, error) {
+	registry.RegisterAuto(func(cfg *config.AppConfig, c *core.Container) (bool, core.Component, error) {
 		return true, api.NewTaskMgmtController(), nil
 	})
 }
--- app/projects/cronjob/internal/registry_ext/service.go
+++ app/projects/cronjob/internal/registry_ext/service.go
@@ -5,18 +5,16 @@ import (
 	"github.com/grand-thief-cash/chaos/app/infra/go/application/core"
 	"github.com/grand-thief-cash/chaos/app/infra/go/application/registry"
 	bizConfig "github.com/grand-thief-cash/chaos/app/projects/cronjob/internal/config"
-	"github.com/grand-thief-cash/chaos/app/projects/cronjob/internal/consts"
 	"github.com/grand-thief-cash/chaos/app/projects/cronjob/internal/executor"
 	"github.com/grand-thief-cash/chaos/app/projects/cronjob/internal/scheduler"
 )
 
 func init() {
 	cronjobCfg := bizConfig.GetBizConfig()
-
-	registry.RegisterAuto(consts.COMP_SVC_EXECUTOR, func(cfg *config.AppConfig, c *core.Container) (bool, core.Component, error) {
+	registry.RegisterAuto(func(cfg *config.AppConfig, c *core.Container) (bool, core.Component, error) {
 		return true, executor.NewExecutor(cronjobCfg.Executor), nil
 	})
-	registry.RegisterAuto(consts.COMP_SVC_SCHEDULER, func(cfg *config.AppConfig, c *core.Container) (bool, core.Component, error) {
+	registry.RegisterAuto(func(cfg *config.AppConfig, c *core.Container) (bool, core.Component, error) {
 		return true, scheduler.NewEngine(cronjobCfg.Scheduler), nil
 	})
 }
