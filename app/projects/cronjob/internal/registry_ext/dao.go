8e84076432c0b1d4931678139d5d9152b997d91c
Chanter-Machine <992400463@qq.com>
2025-10-17 21:53:02 +0800

    - v0.4.2   - Replaced  RegisterAutoInferName with registry.RegisterAuto.

--- app/projects/cronjob/internal/registry_ext/dao.go
+++ app/projects/cronjob/internal/registry_ext/dao.go
@@ -4,15 +4,14 @@ import (
 	"github.com/grand-thief-cash/chaos/app/infra/go/application/config"
 	"github.com/grand-thief-cash/chaos/app/infra/go/application/core"
 	"github.com/grand-thief-cash/chaos/app/infra/go/application/registry"
-	bizConsts "github.com/grand-thief-cash/chaos/app/projects/cronjob/internal/consts"
 	"github.com/grand-thief-cash/chaos/app/projects/cronjob/internal/dao"
 )
 
 func init() {
-	registry.RegisterAuto(bizConsts.COMP_DAO_TASK, func(cfg *config.AppConfig, c *core.Container) (bool, core.Component, error) {
+	registry.RegisterAuto(func(cfg *config.AppConfig, c *core.Container) (bool, core.Component, error) {
 		return true, dao.NewTaskDao("cronjob"), nil
 	})
-	registry.RegisterAuto(bizConsts.COMP_DAO_RUN, func(cfg *config.AppConfig, c *core.Container) (bool, core.Component, error) {
+	registry.RegisterAuto(func(cfg *config.AppConfig, c *core.Container) (bool, core.Component, error) {
 		return true, dao.NewRunDao("cronjob"), nil
 	})
 }
