f3e8eb35cfac3711159969dde7887113798d2028
Chanter-Machine <992400463@qq.com>
2026-05-08 00:00:29 +0800

    - v0.9.1     - Added phoenixA on top-nav bar.

--- app/projects/cthulhu/src/app/core/services/top-nav.service.ts
+++ app/projects/cthulhu/src/app/core/services/top-nav.service.ts
@@ -7,7 +7,8 @@ export class TopNavService {
   private readonly items: TopNavItem[] = [
     { key: 'cronjobs', label: 'Cron Jobs', icon: 'user', path: '/cronjobs' },
     { key: 'artemis', label: 'Artemis', icon: 'bars', path: '/artemis' },
-    { key: 'workbench', label: 'Workbench', icon: 'line-chart', path: '/workbench' }
+    { key: 'workbench', label: 'Workbench', icon: 'line-chart', path: '/workbench' },
+    { key: 'phoenixa', label: 'PhoenixA', icon: 'database', path: '/phoenixa' }
   ];
   getItems(): ReadonlyArray<TopNavItem> { return this.items; }
 }
--- app/projects/cthulhu/src/environments/environment.prod.ts
+++ app/projects/cthulhu/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   cronjobApiBase: 'http://192.168.31.72:8082/api/v1',
   artemisApiBase: 'http://192.168.31.72:8084',
-  phoenixAApiBase: 'http://192.168.31.72:8081'
+  phoenixAApiBase: 'http://192.168.31.72:8085'
 };
